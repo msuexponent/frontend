5e4cca0fb22a5bdfc9405412c6b07736894932c7
Rishad Bharucha <rb2102@gmail.com>
2016-01-04 11:13:20 -0700

    Clean up indentation in library/sections

--- library/sections/common.php
+++ library/sections/common.php
@@ -1,72 +1,72 @@
 <?php
 
 /**
-	Get list of categories as labels
-	**/
-	function get_category_list() {
-		$categories = get_the_category();
-		$separator = ' ';
-		$output = '';
-		if($categories){
-			foreach($categories as $category) {
-				$output .= '<a class="round radius label" href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name.'</a>'.$separator;
-			}
-			echo trim($output, $separator);
-		}	
-	}
-
-/**
-	Get images attached to post
-	**/
-	function get_post_attachments($args=array()) {
-		// Global post variable
-		global $post;
-
-		// Default attributes
-		$defaults = array(
-			'numberposts'		=> -1,
-			'order'				=> 'ASC',
-			'orderby'			=> 'menu_order',
-			'post_mime_type'	=> 'image',
-			'post_parent'		=>  $post->ID,
-			'post_type'			=> 'attachment',
-			);
-
-		// Parse arguments
-		$args = wp_parse_args($args,$defaults);
-
-		// Return images
-		return get_posts($args);
-	}
-
-/**
-	Prints section label
-	**/
-	function get_label($label) {
-		echo '<a href="' . site_url() . '/' . $label . '/"><div class="category-label">' . $label . '</div></a>';
-	}
-
-/**
-	The "more posts" arrow button
-	**/
-	function get_more_posts($cat_name) {
-		if ($cat_name == "multimedia") {
-			echo '<a class="more-posts" href="https://www.youtube.com/user/ASMSUExponent" target="_blank">→</a>';
+ * Get list of categories as labels
+ */
+function get_category_list() {
+	$categories = get_the_category();
+	$separator = ' ';
+	$output = '';
+	if( $categories ){
+		foreach( $categories as $category ) {
+			$output .= '<a class="round radius label" href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name.'</a>'.$separator;
 		}
-		else {
-			echo '<a class="more-posts" href="' . site_url() . '/' . $cat_name . '/">→</a>';	
-		}	
+		echo trim( $output, $separator );
 	}
+}
 
 /**
-	Check if the Co-Authors PLus plugin is enabled for multi-author support
-	**/
-	function coauthors_author_info() {
-		if ( function_exists( 'coauthors_posts_links' ) ) {
-			coauthors_posts_links();
-		} else {
-			the_author_posts_link();
-		}
-	}
+ * Get images attached to post
+ */
+function get_post_attachments( $args=array() ) {
+	// Global post variable
+	global $post;
 
-	?>
+	// Default attributes
+	$defaults = array(
+		'numberposts'		=> -1,
+		'order'				=> 'ASC',
+		'orderby'			=> 'menu_order',
+		'post_mime_type'	=> 'image',
+		'post_parent'		=>  $post->ID,
+		'post_type'			=> 'attachment',
+	);
+
+	// Parse arguments
+	$args = wp_parse_args( $args,$defaults );
+
+	// Return images
+	return get_posts( $args );
+}
+
+/**
+ * Prints section label
+ */
+function get_label( $label ) {
+	echo '<a href="' . site_url() . '/' . $label . '/"><div class="category-label">' . $label . '</div></a>';
+}
+
+/**
+ * The "more posts" arrow button
+**/
+function get_more_posts( $cat_name ) {
+	if ( $cat_name == "multimedia" ) {
+		echo '<a class="more-posts" href="https://www.youtube.com/user/ASMSUExponent" target="_blank">→</a>';
+	}
+	else {
+		echo '<a class="more-posts" href="' . site_url() . '/' . $cat_name . '/">→</a>';
+	}
+}
+
+/**
+ * Check if the Co-Authors PLus plugin is enabled for multi-author support
+**/
+function coauthors_author_info() {
+	if ( function_exists( 'coauthors_posts_links' ) ) {
+		coauthors_posts_links();
+	} else {
+		the_author_posts_link();
+	}
+}
+
+?>
--- library/sections/sports.php
+++ library/sections/sports.php
@@ -5,144 +5,144 @@ $excluded_sports_1;
 $excluded_sports_2;
 
 /**
-	Returns the two latest sports post titles and thumbnails
-	**/
-	function get_latest_sports_posts() {
+ * Returns the two latest sports post titles and thumbnails
+**/
+function get_latest_sports_posts() {
 
-		$args = array (
-			'posts_per_page' => 2,
-			'category_name' => 'sports',
-			'meta_key' => '_thumbnail_id' 
-			);
+	$args = array (
+	'posts_per_page' => 2,
+	'category_name' => 'sports',
+	'meta_key' => '_thumbnail_id'
+);
 
-		$the_query = new WP_Query($args);
-		$i = 1;
+$the_query = new WP_Query( $args );
+$i = 1;
 
-		if ( $the_query->have_posts() ) {
-			echo '<ul class="large-block-grid-2 medium-block-grid-2 small-block-grid-1">';
-			while ( $the_query->have_posts() ) {
-				$the_query->the_post();
-				// Set excluded posts
-				if ($i == 1) {
-					$GLOBALS['excluded_sports_1'] = get_post()->ID;
-				}
-				else if ($i == 2) {
-					$GLOBALS['excluded_sports_2'] = get_post()->ID;
-				}
-				// List element
-				echo '<li><h5>';
-				// Link
-				echo '<a href="' . get_permalink( get_post()->ID ) . '" title="' . esc_attr( get_post()->post_title ) . '">';
-				// Thumbnail
-				echo get_the_post_thumbnail( get_post()->ID, 'size-thumbnail-medium', array('class'	=> "get_two_latest") );
-				echo '</a><br/>';
-				// Subcategory
-				echo '<span class="subcategory-name">';
-				$category = get_the_category(); 
-				foreach((get_the_category()) as $childcat) {
-					if (cat_is_ancestor_of(13, $childcat)) {
-						echo '<a href="'.get_category_link($childcat->cat_ID).'">';
-						echo $childcat->cat_name . '</a>';
-						echo ' · ';
-					}
-				}
-				echo '</span>';
-				// Link
-				echo '<a href="' . get_permalink( get_post()->ID ) . '" title="' . esc_attr( get_post()->post_title ) . '">';
-				// Title
-				echo '<span class="headline">' . get_the_title( get_post()->ID ) . '</span>';
-				echo '</a></h5>';
-				echo '</li>';
-				$i++;
+if ( $the_query->have_posts() ) {
+	echo '<ul class="large-block-grid-2 medium-block-grid-2 small-block-grid-1">';
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();
+		// Set excluded posts
+		if ( $i == 1 ) {
+			$GLOBALS['excluded_sports_1'] = get_post()->ID;
+		}
+		else if ( $i == 2 ) {
+			$GLOBALS['excluded_sports_2'] = get_post()->ID;
+		}
+		// List element
+		echo '<li><h5>';
+		// Link
+		echo '<a href="' . get_permalink( get_post()->ID ) . '" title="' . esc_attr( get_post()->post_title ) . '">';
+		// Thumbnail
+		echo get_the_post_thumbnail( get_post()->ID, 'size-thumbnail-medium', array( 'class' => "get_two_latest" ) );
+		echo '</a><br/>';
+		// Subcategory
+		echo '<span class="subcategory-name">';
+		$category = get_the_category();
+		foreach( ( get_the_category() ) as $childcat ) {
+			if ( cat_is_ancestor_of( 13, $childcat ) ) {
+				echo '<a href="' . get_category_link( $childcat->cat_ID ) . '">';
+				echo $childcat->cat_name . '</a>';
+				echo ' · ';
 			}
-			echo '</ul>';
 		}
-		else {
-			// no posts found
-		}
-
-		wp_reset_postdata();
-
+		echo '</span>';
+		// Link
+		echo '<a href="' . get_permalink( get_post()->ID ) . '" title="' . esc_attr( get_post()->post_title ) . '">';
+		// Title
+		echo '<span class="headline">' . get_the_title( get_post()->ID ) . '</span>';
+		echo '</a></h5>';
+		echo '</li>';
+		$i++;
 	}
+	echo '</ul>';
+}
+else {
+	// no posts found
+}
+
+wp_reset_postdata();
+
+}
 
 /**
-	Returns sports posts without thumbnails as a block grid
-	**/
-	function get_sports_list() {
+ * Returns sports posts without thumbnails as a block grid
+**/
+function get_sports_list() {
 
-		$exclude_ids = array(
-			$GLOBALS['excluded_sports_1'],
-			$GLOBALS['excluded_sports_2']
-			);
+	$exclude_ids = array(
+		$GLOBALS['excluded_sports_1'],
+		$GLOBALS['excluded_sports_2']
+	);
 
-		$args = array (
-			'posts_per_page' => 6,
-			'category_name' => 'sports',
-			'post__not_in' => $exclude_ids
-			);
+	$args = array (
+	'posts_per_page' => 6,
+	'category_name' => 'sports',
+	'post__not_in' => $exclude_ids
+);
 
-		$the_query = new WP_Query($args);
+$the_query = new WP_Query( $args );
 
-		if ( $the_query->have_posts() ) {
-			echo '<ul class="large-block-grid-2 medium-block-grid-2">';
-			while ( $the_query->have_posts() ) {
-				$the_query->the_post();
-				// List element
-				echo '<li><h6>';
-				// Subcategory
-				echo '<span class="subcategory-name">';
-				$category = get_the_category(); 
-				foreach((get_the_category()) as $childcat) {
-					if (cat_is_ancestor_of(13, $childcat)) {
-						echo '<a href="'.get_category_link($childcat->cat_ID).'">';
-						echo $childcat->cat_name . '</a>';
-						echo ' · ';
-					}
-				}
-				echo '</span>';
-				// Link
-				echo '<a href="' . get_permalink( get_post()->ID ) . '" title="' . esc_attr( get_post()->post_title ) . '">';
-				// Title
-				echo '<span class="headline">' . get_the_title( get_post()->ID ) . '</span>';
-				echo '</a></h6></li>';
+if ( $the_query->have_posts() ) {
+	echo '<ul class="large-block-grid-2 medium-block-grid-2">';
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();
+		// List element
+		echo '<li><h6>';
+		// Subcategory
+		echo '<span class="subcategory-name">';
+		$category = get_the_category();
+		foreach( ( get_the_category() ) as $childcat ) {
+			if ( cat_is_ancestor_of( 13, $childcat ) ) {
+				echo '<a href="' . get_category_link( $childcat->cat_ID ).'">';
+				echo $childcat->cat_name . '</a>';
+				echo ' · ';
 			}
-			echo '</ul>';
 		}
-		else {
-			// no posts found
-		}
-
-		wp_reset_postdata();
+		echo '</span>';
+		// Link
+		echo '<a href="' . get_permalink( get_post()->ID ) . '" title="' . esc_attr( get_post()->post_title ) . '">';
+		// Title
+		echo '<span class="headline">' . get_the_title( get_post()->ID ) . '</span>';
+		echo '</a></h6></li>';
 	}
+	echo '</ul>';
+}
+else {
+	// no posts found
+}
+
+wp_reset_postdata();
+}
 
 /**
-	Returns sports posts without thumbnails as a block grid, for the mobile view
-	**/
-	function get_sports_list_mobile() {
+ * Returns sports posts without thumbnails as a block grid, for the mobile view
+**/
+function get_sports_list_mobile() {
 
-		$args = array (
-			'posts_per_page' => 6,
-			'category_name' => 'sports',
-			);
+	$args = array (
+	'posts_per_page' => 6,
+	'category_name' => 'sports',
+);
 
-		$the_query = new WP_Query($args);
+$the_query = new WP_Query( $args );
 
-		if ( $the_query->have_posts() ) {
-			echo '<ul class="no-bullet">';
-			while ( $the_query->have_posts() ) {
-				$the_query->the_post();
-				// Link
-				echo '<li><h6><a class="headline" href="' . get_permalink( get_post()->ID ) . '" title="' . esc_attr( get_post()->post_title ) . '">';	
-				// Title
-				echo get_the_title( get_post()->ID ) . '</a></h6></li>';
-			}
-			echo '</ul>';
-		}
-		else {
-			// no posts found
-		}
-
-		wp_reset_postdata();
+if ( $the_query->have_posts() ) {
+	echo '<ul class="no-bullet">';
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();
+		// Link
+		echo '<li><h6><a class="headline" href="' . get_permalink( get_post()->ID ) . '" title="' . esc_attr( get_post()->post_title ) . '">';
+		// Title
+		echo get_the_title( get_post()->ID ) . '</a></h6></li>';
 	}
+	echo '</ul>';
+}
+else {
+	// no posts found
+}
 
-	?>
+wp_reset_postdata();
+}
+
+?>
