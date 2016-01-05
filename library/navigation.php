f012c72dbf49ab10027f1341dfb6dcbeebdbc5ed
Rishad Bharucha <rb2102@gmail.com>
2016-01-05 13:20:04 -0700

    Top bar styling

--- library/navigation.php
+++ library/navigation.php
@@ -9,10 +9,28 @@
  */
 
 register_nav_menus(array(
+	'top-bar-l' => 'Left Top Bar',
 	'top-bar-r' => 'Right Top Bar',
 	'mobile-nav' => 'Mobile',
 ));
 
+/**
+ * Desktop navigation - left top bar
+ * http://codex.wordpress.org/Function_Reference/wp_nav_menu
+ */
+if ( ! function_exists( 'foundationpress_top_bar_l' ) ) {
+	function foundationpress_top_bar_l() {
+	    wp_nav_menu(array(
+        'container' => false,                           // Remove nav container
+        'menu_class' => 'dropdown menu',           			// Adding custom nav class
+				'items_wrap'     => '<ul id="%1$s" class="%2$s show-for-medium" data-dropdown-menu>%3$s</ul>',
+        'theme_location' => 'top-bar-l',                // Where it's located in the theme
+        'depth' => 3,                                   // Limit the depth of the nav
+        'fallback_cb' => false,                         // Fallback function (see below)
+        'walker' => new Foundationpress_Top_Bar_Walker(),
+	    ));
+	}
+}
 
 /**
  * Desktop navigation - right top bar
