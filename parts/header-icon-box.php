9d4c935eeaf808381e4939d83c4d2c583fa5a756
Rishad Bharucha <rb2102@gmail.com>
2016-01-10 21:00:55 -0700

    Add YouTube link to icon box

--- parts/header-icon-box.php
+++ parts/header-icon-box.php
@@ -1,23 +1,29 @@
 <div class="icon-box">
-    <a data-toggle="search-dropdown" target="_blank">
+    <a data-toggle="search-dropdown" target="_blank" title="Search">
         <span class="fa-stack fa-lg">
             <i class="fa fa-circle-thin fa-stack-2x"></i>
             <i class="fa fa-search fa-stack-1x"></i>
         </span>
     </a>
-    <a href="https://facebook.com/msuexponent" target="_blank">
+    <a href="https://www.youtube.com/user/ASMSUExponent" target="_blank" title="YouTube">
+        <span class="fa-stack fa-lg">
+            <i class="fa fa-circle-thin fa-stack-2x"></i>
+            <i class="fa fa-youtube-play fa-stack-1x"></i>
+        </span>
+    </a>
+    <a href="https://facebook.com/msuexponent" target="_blank" title="Facebook">
         <span class="fa-stack fa-lg">
             <i class="fa fa-circle-thin fa-stack-2x"></i>
             <i class="fa fa-facebook fa-stack-1x"></i>
         </span>
     </a>
-    <a href="https://twitter.com/MSUExpo" target="_blank">
+    <a href="https://twitter.com/MSUExpo" target="_blank" title="Twitter">
         <span class="fa-stack fa-lg">
             <i class="fa fa-circle-thin fa-stack-2x"></i>
             <i class="fa fa-twitter fa-stack-1x"></i>
         </span>
     </a>
-    <a href="https://instagram.com/MSUExpo" target="_blank">
+    <a href="https://instagram.com/MSUExpo" target="_blank" title="Instagram">
         <span class="fa-stack fa-lg">
             <i class="fa fa-circle-thin fa-stack-2x"></i>
             <i class="fa fa-instagram fa-stack-1x"></i>
