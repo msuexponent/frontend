6787e9f7e52fbb6d4b77381b1c2dbf62ad96b0a7
Rishad Bharucha <rb2102@gmail.com>
2016-01-11 07:01:19 -0700

    Add flex video styling to Multimedia video

--- library/sections/multimedia.php
+++ library/sections/multimedia.php
@@ -10,7 +10,9 @@
  * Get the latest multimedia video
  */
 function get_multimedia_video() {
+    echo '<div class="flex-video">';
     echo '<iframe width="100%" height="156" src="http://www.youtube.com/embed?max-results=1&controls=1&showinfo=0&rel=0&listType=user_uploads&list=asmsuexponent" frameborder="0" allowfullscreen="allowfullscreen"></iframe>';
+    echo '</div>';    
 }
 
 /**
