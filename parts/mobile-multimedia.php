504965127001adfef9b3faaf95b44548086d1603
Tyler Foulkes <tylerfoulkes@Tyler-Foulkess-MacBook.local>
2016-04-13 17:31:22 -0600

    Added youtube channel to mobile-multimedia.php file

--- parts/mobile-multimedia.php
+++ parts/mobile-multimedia.php
@@ -1,7 +1,12 @@
 <!-- // Multimedia // -->
 
 <!-- Category label -->
-<?php get_label('multimedia'); ?>
+<?php //get_label('multimedia'); ?>
+<h5>
+	<strong>
+		<a href="https://www.youtube.com/channel/UCP51_C7GUpefVEbAQHPCMew?nohtml5=False">multimedia</a>
+	</strong>
+</h5>
 <hr>
 <!-- Latest video -->
 <?php get_multimedia_video(); ?>
