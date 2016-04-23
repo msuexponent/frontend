cc07bc1d05574da9e62359c2b61f903e5f5d4980
Tyler Foulkes <tylerfoulkes@TylerFosMacBook.PK5001Z>
2016-04-22 22:49:08 -0600

    Added function to functions.php for search capabilities

--- functions.php
+++ functions.php
@@ -63,4 +63,16 @@ require_once( 'library/sections/sports.php' );
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/protocol-relative-theme-assets.php' );
 
+### Function: Add Author Custom Fields
+add_action('publish_post', 'add_author_fields');
+function add_author_fields($post_ID) {
+global $wpdb;
+	$user_id = $wpdb->get_var("SELECT post_author FROM $wpdb->posts WHERE ID = $post_ID");
+	$first_name = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE meta_key = 'first_name' AND user_id = $user_id");
+	$last_name = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE meta_key = 'last_name' AND user_id = $user_id");
+	$user_name = $wpdb->get_var("SELECT user_login FROM $wpdb->users WHERE ID = $user_id");
+	add_post_meta($post_ID, 'author_realname', $first_name.' '.$last_name, true);
+	add_post_meta($post_ID, 'author_username', $user_name, true);
+}
+
 ?>
