370f654c352797cdb02c4177a32a4b6fc5f71fa0
Colin Marshall <colin.michael.marshall@gmail.com>
2016-05-11 10:02:51 -0600

    Removed repeated search dropdown

--- header.php
+++ header.php
@@ -42,10 +42,10 @@
 
 		<div id="logo">
 			<div class="row">
-				<div class="small-12 medium-8 columns">
+				<div class="small-12 medium-7 columns">
 					<a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri();?>/assets/images/site/exponent-logo.png"></a>
 				</div>
-				<div class="small-12 medium-4 columns">
+				<div class="small-12 medium-5 columns">
 					<?php get_template_part( 'parts/header', 'icon-box' ); ?>
 				</div>
 			</div>
