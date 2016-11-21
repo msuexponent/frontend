2744b96de06ef2ff51adc15944d77caf7f3639ad
Tyler Foulkes <tylerfoulkes@Tyler-Foulkess-MacBook.local>
2016-11-20 22:38:40 -0700

    Removed specific version for main css sheet in the wp_enqueue_style function call

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var del         = require('del');
 
 // Enter URL of your local server here
 // Example: 'http://localwebsite.dev'
-var URL = 'https://localhost';
+var URL = 'http://localhost/exponent_code/live_site_structure';
 
 // Check for --production flag
 var isProduction = !!(argv.production);
